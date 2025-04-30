121e170d0aef14436ce197f21a23b384e54ae9f3
Ajay Sah from TB <ajaysah884@gmail.com>
2025-04-30 11:49:45 +0530

    dependecy issue

--- src/main/java/org/example/loadbalancer/model/Server.java
+++ src/main/java/org/example/loadbalancer/model/Server.java
@@ -1,17 +1,20 @@
 package org.example.loadbalancer.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
 @Data
+@Entity
+@NoArgsConstructor
 public class Server extends BaseModel{
 
     private String serverName;
 
-    private String serverUrl;
+    @Enumerated(EnumType.STRING)
+    private HealthStatus healthStatus;
 
-    private HealthStatus healthStatus = HealthStatus.ACTIVE;
+    private String serverUrl;
 
     public Server(String serverName, HealthStatus healthStatus, String serverUrl) {
         this.serverName = serverName;
@@ -19,6 +22,28 @@ public class Server extends BaseModel{
         this.serverUrl = serverUrl;
     }
 
-    public Server() {
+    // ✅ Getter method for serverName
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public HealthStatus getHealthStatus() {
+        return healthStatus;
+    }
+
+    public void setHealthStatus(HealthStatus healthStatus) {
+        this.healthStatus = healthStatus;
     }
 }
--- src/main/java/org/example/loadbalancer/service/RoundRobinService.java
+++ src/main/java/org/example/loadbalancer/service/RoundRobinService.java
@@ -22,15 +22,14 @@ public class RoundRobinService {
     public void startCheck() throws InterruptedException {
 
         List<Server> allServers = serverRespository.findAll();
-
         if(allServers.size() == 0){
             throw new NoActiveServers("No active servers found");
         }
-
         while(true){
             counter = (counter + 1)%allServers.size();
             Server server = allServers.get(counter);
             Boolean status = loadBalancerService.isServerUp(server.getServerUrl(), server);
+
             if(status){
             }else{
                 System.out.println(server.getServerName() + " is not up");
